faf78cbbf11e49712ac39ecf6d016b3b2df8757e
李云涛 <hydra.li@idss-cn.com>
2022-07-22 14:52:09 +0800

    paramsNewInstance

--- src/main/java/com/el/eldevops/service/IParamsInstanceService.java
+++ src/main/java/com/el/eldevops/service/IParamsInstanceService.java
@@ -1,9 +1,20 @@
 package com.el.eldevops.service;
 
+import com.el.eldevops.model.ParamsInstanceVO;
+
 /**
  * @author YunTao.Li
  * @description:
  * @date 2022/7/21 10:34
  */
 public interface IParamsInstanceService {
+
+    /**
+     * @description 新增服务参数实例
+
+     * @return void
+     * @author YunTao.Li
+     * @date 2022/7/22 14:49
+     */
+    public void paramsNewInstance(ParamsInstanceVO paramsInstanceVO) throws Exception;
 }
--- src/main/java/com/el/eldevops/service/impl/ParamsInstanceServiceImpl.java
+++ src/main/java/com/el/eldevops/service/impl/ParamsInstanceServiceImpl.java
@@ -1,8 +1,14 @@
 package com.el.eldevops.service.impl;
 
+import com.el.eldevops.mapper.ParamsInstanceMapper;
+import com.el.eldevops.model.ParamsInstanceEntity;
+import com.el.eldevops.model.ParamsInstanceVO;
 import com.el.eldevops.service.IParamsInstanceService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * @author YunTao.Li
@@ -12,4 +18,15 @@ import org.springframework.stereotype.Component;
 @Primary
 @Component
 public class ParamsInstanceServiceImpl implements IParamsInstanceService {
+
+    @Resource
+    private ParamsInstanceMapper soarParamsDefEntity;
+
+
+    @Override
+    public void paramsNewInstance(ParamsInstanceVO paramsInstanceVO) throws Exception {
+        ParamsInstanceEntity paramsInstanceEntity = new ParamsInstanceEntity();
+        BeanUtils.copyProperties(paramsInstanceVO, paramsInstanceEntity);
+        soarParamsDefEntity.insert(paramsInstanceEntity);
+    }
 }
